185e083ad15bb3971caad2b3d986daf232729d43
Kashi Samaraweera <kashi@kashmaniac.com.au>
2015-10-28 23:18:43 +0100

    Bug in build process that failed to copy assets to the build directory.

--- gulp-tasks/build.js
+++ gulp-tasks/build.js
@@ -40,6 +40,7 @@ gulp.task('build', function(callback) {
         return runSequence(
             'build:clean',
             [
+                'build:ext:assets',
                 'build:ext:css',
                 'build:ext:js',
                 'build:ext:html'
